261c93cf82d61abca0e64fb007c51584ac8a6980
deepika <javarules75@gmail.com>
2026-04-24 21:31:43 +0530

    Countries.js file is created and the api call is done in drop down

---
+++ src/Countries.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
+
+
+function Countries() {
+  const [countries,setCountries]  = useState([]);
+
+  useEffect(() =>{
+    axios.get('https://restcountries.com/v3.1/all?fields=name,capital,currencies').then((response) => {
+      setCountries(response.data);
+    }).catch((error) => {
+      console.error('Error fetching countries:', error);
+    });
+  },[])
+  return (
+    <div>
+        <select>
+            {countries.map((country) => (
+              <option key={country.name.common} value={country.name.common}>
+                {country.name.common}
+              </option>
+            ))}
+        </select>
+      
+    </div>
+  )
+}
+
+export default Countries
